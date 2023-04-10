47ec26d546ac57ba967f549a5892350c391cc97f
hd-buddy <117499276+hd-buddy@users.noreply.github.com>
2023-04-10 16:16:20 +0530

    Update forms.sql

--- forms.sql
+++ forms.sql
@@ -30,17 +30,8 @@ INSERT INTO form VALUES(
     'dhruv@gmail.com',
     '26-MAR-2003'
 );
-CREATE TABLE my_sequence (
-  sequence_value number
-);
-INSERT INTO my_sequence (sequence_value) VALUES (0);
-CREATE TRIGGER my_table_trigger
-BEFORE INSERT ON movies
-FOR EACH ROW
-BEGIN
-  SELECT my_sequence.sequence_value + 1 INTO :new.id FROM dual;
-  UPDATE my_sequence SET sequence_value = :new.id;
-END;
+drop table movies;
+
 create table movies(
    movie_id int primary key,
    title varchar2(256),
@@ -49,3 +40,30 @@ create table movies(
    language varchar2(20),
    release_date date,
    genre varchar2(20));
+   
+
+CREATE SEQUENCE movies_seq
+start with 1
+maxvalue 9999999
+minvalue 1
+CYCLE
+NOCACHE
+NOORDER;
+
+
+create or replace trigger A_trigger
+    before insert on movies
+    for each ROW
+BEGIN
+    if :new.movie_id is null THEN
+        :new.movie_id := movies_seq.nextval;
+    end if;
+END;
+/
+insert into movies values(DEFAULT,'Demon slayer','anime',120,'japenese','25-MAR-2023','Action');
+insert into movies values(DEFAULT,'Avatar 2','nice cinematogrphy',180,'English','01-MAR-2023','Sci-fi');
+insert into movies values(DEFAULT,'Jurasic World','dinosaurs',150,'Hinglish','01-APR-2023','Biology');
+insert into movies values(DEFAULT,'Vikram','dinosaurs',150,'Hinglish','01-APR-2023','Biology');
+insert into movies values(DEFAULT,'Fire Starter','dinosaurs',150,'Hinglish','01-APR-2023','Biology');
+insert into movies values(DEFAULT,'Jurasic World','dinosaurs',150,'Hinglish','01-APR-2023','Biology');
+select * from movies;
